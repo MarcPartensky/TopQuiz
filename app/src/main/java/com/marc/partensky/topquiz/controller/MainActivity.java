3edb6daf52c84abec7ecdf1d3f666fd190951720
Marc Partensky <marc.partensky@gmail.com>
2019-12-10 17:57:30 +0100

    added firstname to the user object

--- app/src/main/java/com/marc/partensky/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/marc/partensky/topquiz/controller/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mUser = new User();
+
         mGreetingText = findViewById(R.id.activity_main_greeting_txt);
         mNameInput = findViewById(R.id.activity_main_name_input);
         mPlayButton = findViewById(R.id.activity_main_play_btn);
@@ -52,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String firstname = mNameInput.getText().toString();
+                mUser.setFirstName(firstname);
                 Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(gameActivityIntent);
             }
